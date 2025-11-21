00279ba0a261d5d61d316671a3365cf67765a937
Frederick <sedemcopper@gmail.com>
2025-11-21 12:57:46 +0000

    Add project files.

---
+++ PoultryFarmManagement.DAL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoultryFarmManagement.DAL
+{
+    public class Class1
+    {
+    }
+}
